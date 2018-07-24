d9c55f2a3ac9fe79c69d94430b5b7694e31d2832
yaozhonghui <yaozhonghui@cbpm-kexin.com>
2018-07-24 20:08:43 +0800

    show bug
    没显示全

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from MDP import MDP
 from ValueIteration import VI, vi_wrapper
-from visualization import visualize_values, visualize_map, interact
+from visualization import visualize_values, visualize_map
 from generator import make_small_env, make_large_env
 
